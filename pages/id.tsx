0b42174d6380e366622ce1358fdc2e806575a32d
gty <gefyoung@gmail.com>
2022-02-01 21:38:12 -0700

    trying to conditionally render based on date

--- pages/id.tsx
+++ pages/id.tsx
@@ -1,80 +1,68 @@
 import ical from 'node-ical'
 import { useState } from 'react'
+import moment from 'moment'
+
+interface FlipEvent {
+  dayBegins: number
+  dayEnds: number
+  start: number
+  duration: number
+  startDate: Date
+  summary: string,
+  className: string
+}
+
 
 export default function Id({ data }: any) {
 
-  const [selectedEventState, setSelectedEventState] = useState(null)
-
-  function groupBy(objectArray: any, property: any) {
-    return objectArray.reduce(function (acc: any, obj: any) {
-      let key = obj[property]
-      if (!acc[key]) {
-        acc[key] = []
-      }
-      acc[key].push(obj)
-      return acc
-    }, [])
-  }
-
-  const groupedDays = groupBy(data, 'dayBegins')
-  const keylessArray: any[] = []
-  Object.values(groupedDays).forEach((day: any) => {
-    keylessArray.push(day)
+  const [selectedEventState, setSelectedEventState] = useState({
+    summary: null,
+    className: null,
+    startDate: null
   })
 
-  let day = 84000
-
-  interface FlipEvent {
-    dayBegins: number
-    dayEnds: number
-    start: number
-    duration: number
-    startDate: Date
-    summary: string
+  const selectFlip = (e: FlipEvent) => {
+    console.log(e.startDate)
+    // setSelectedEventState({ ...e, startDate: e.startDate.getDate() })
   }
 
   const FlipComponent = ({ flipEvent }: any) => {
-    const eventSpan: any[] = []
-    
-    return <div  onClick={() => setSelectedEventState(flipEvent.summary)} className="flex">
+    return <div key={flipEvent.starTime} onClick={() => selectFlip(flipEvent)}>
       <div className={flipEvent.className}>
-      {/* {
-        eventSpan.map((sliver) => {
-          return <span 
-          className={sliver.className}
-        ></span>
-        })
-      } */}
     </div></div>
   }
 
   const DayComponent = ({ day }: any) => {
-    return <div className="flex my-20 overflow-hidden max-w-prose">{
+    return <><div className="flex my-20 overflow-hidden">{
       day.map((flipEvent: FlipEvent) =>
         <FlipComponent key={flipEvent.start}  flipEvent={flipEvent} />
-      )}</div>
+      )}</div></>
   }
 
   return (
     <div className="mx-10">
-      <div>{selectedEventState}</div>
-      {keylessArray.map((day) => 
-        <div key={keylessArray.indexOf(day)}>
-        <DayComponent key={day} day={day} />
+      {
+        Object.entries(data).map(([key, day]) => 
+        <>
+        <div className='flex '>
+          {new Date(parseInt(key)).toLocaleDateString() + ' '} 
+          {new Date(parseInt(key)).toLocaleString('en-us', {  weekday: 'long' })}
         </div>
-      )}
+        { selectedEventState.startDate === new Date(parseInt(key)) && <div className='flex '>
+          { selectedEventState.summary + " " + selectedEventState.className }
+        </div> }
+          <DayComponent key={day} day={day} />
+      </>
+        )
+      }
     </div>
   )
-
-  // return (
-  //       <div key={keylessArray.indexOf(day)}>
-  //         <div>{selectedEventState}</div>
-  //       <DayComponent key={day} day={keylessArray[2]} />
-  //       </div>
-  // )
 }
 
-const returnColor = (summary: any) => {
+
+const userTZ = 'America/Denver'
+
+function returnColor(summary: string){
   switch (summary) {
     case "Jerkin": return "bg-red-600"
     case "Learning": return "bg-yellow-600"
@@ -83,44 +71,37 @@ const returnColor = (summary: any) => {
     case "Weed": return "bg-amber-600"
     case "Socializing": return "bg-lime-600"
     case "Beer": return "bg-teal-600"
-    case "Working out": return "bg-violet-600"
+    case "Working out": return "bg-blue-600"
     case "Insta/tv/youtub": return "bg-pink-600"
     case "Shop/Chores": return "bg-rose-600"
     case "Skiing": return "bg-cyan-600"
-    case "Norski": return "bg-indigo-600"
+    case "Norski": return "bg-black"
     default:
       "bg-white"
   }
 }
 
-const returnWidth = (duration: any) => {
+function returnWidth(duration: number) {
   let minutes = duration / 60000
-  let height = " h-8"
-  const min4 = Math.ceil(minutes / 4)
-  console.log("min4s", min4)
-  return `w-${min4} h-8`
-  
-
-  // if (minutes < 2) {
-  //   return "w-0.5" + height
-  // } else if (minutes < 4) {
-  //   return `w-1` + height
-  // } else if (minutes < 8) {
-  //   return `w-2` + height
-  // } else if (minutes < 16) {
-  //   return `w-4` + height
-  // } else if (minutes < 32) {
-  //   return `w-8` + height
-  // } else if (minutes < 64) {
-  //   return `w-16` + height
-  // } else if (minutes < 128) {
-  //   return `w-32` + height
-  // } else if (minutes < 256) {
-  //   return `w-48` + height
-  // } else if (minutes > 256) {
-  //   return `w-96` + height
+  let part180 = (minutes / 8) * 10
+  let rounded5 = (Math.round(part180/5)*5) / 10
+  const not0 = rounded5 === 0 ? rounded5 + 0.5 : rounded5
+  // if (rounded5 < 72) {
+    return `w-${not0}ch h-8`
+  // } else {
+  //   return 'w-72 h-8'
   // }
-  
+}
+
+function groupByDays(objectArray: any) {
+  return objectArray.reduce(function (acc: any, obj: any) {
+    let key = obj.dayBegins
+    if (!acc[key]) {
+      acc[key] = []
+    }
+    acc[key].push(obj)
+    return acc
+  }, {})
 }
 
 function durate(sorted: any){
@@ -129,6 +110,20 @@ function durate(sorted: any){
     sorted[i].className = returnWidth(sorted[i].duration) + " " + returnColor(sorted[i].summary)
   }
   return sorted
+}
+
+// add up a day's of duration
+
+function sortFlips(arr: any){
+  return arr.sort((a: FlipEvent, b: FlipEvent) => {
+    if (a.start < b.start) {
+      return -1
+    } else if (b.start < a.start) {
+      return 1
+    } else {
+      return 0
+    }
+  })
 }
 
 
@@ -140,42 +135,35 @@ export async function getStaticProps() {
 
     for (const calEvent of Object.values(parsedICAL)) {
 
-      const userTZ = 'America/Denver'
-
       
 
       if (calEvent.start instanceof Date) {
         const utcDate = new Date(calEvent.start)
         const convertedTZ = utcDate.toLocaleString("en-US", { timeZone: userTZ })
+        console.log(convertedTZ)
         const convertedDate = new Date(convertedTZ)
         const startTime = convertedDate.getTime()
 
         const dayBegins = Date.parse(convertedDate.toDateString())
+
 
         let newShit = {
           dayBegins: dayBegins,
           dayEnds: dayBegins + 8640000,
           start: startTime,
           duration: null,
-          startDate: convertedTZ,
+          startDate: calEvent.start,
           summary: calEvent.summary,
           className: null
         }
         eventArray.push(newShit)
       }
     }
+    const sorted = sortFlips(eventArray)
+    const withDuration = durate(sorted)
+    const groupedDays = groupByDays(withDuration)
 
-    const sorted = eventArray.sort((a, b) => {
-      if (a.start < b.start) {
-        return -1
-      } else if (b.start < a.start) {
-        return 1
-      } else {
-        return 0
-      }
-    })
-
-    return { props: { data: durate(sorted) }, revalidate: 1 }
+    return { props: { data: groupedDays }, revalidate: 1 }
   } catch (err) {
     return { props: { data: null }, revalidate: 1 }
   }
