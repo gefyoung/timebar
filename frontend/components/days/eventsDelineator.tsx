b15ff6fe54b320c0481768dd5cfb6cc3d51bed88
gty <gefyoung@gmail.com>
2022-06-09 09:12:33 -0600

    added numbers to deliniators :

--- frontend/components/days/eventsDelineator.tsx
+++ frontend/components/days/eventsDelineator.tsx
@@ -3,50 +3,58 @@ import Image from "next/dist/client/image"
 const EventsDelineator = () => {
   return (
     <div className="grid grid-cols-16">
-      <div className="col-span-1">
-      <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" />
+      <div className="col-span-1 mt-2 text-xs">
+      {/* <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" /> */}
+      7
       </div>
       <div className="col-span-1">
       <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" />
       </div>
-      <div className="col-span-1">
-      <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" />
+      <div className="col-span-1 mt-2 text-xs">
+      {/* <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" /> */}
+      9
       </div>
       <div className="col-span-1">
       <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" />
       </div>
-      <div className="col-span-1">
-      <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" />
+      <div className="col-span-1 mt-2 text-xs">
+      {/* <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" /> */}
+      11
       </div>
       <div className="col-span-1">
       <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" />
       </div>
-      <div className="col-span-1">
-      <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" />
+      <div className="col-span-1 mt-2 text-xs">
+      {/* <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" /> */}
+      1
       </div>
       <div className="col-span-1">
       <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" />
       </div>
-      <div className="col-span-1">
-      <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" />
+      <div className="col-span-1 mt-2 text-xs">
+      {/* <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" /> */}
+      3
       </div>
       <div className="col-span-1">
       <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" />
       </div>
-      <div className="col-span-1">
-      <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" />
+      <div className="col-span-1 mt-2 text-xs">
+      {/* <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" /> */}
+      5
       </div>
       <div className="col-span-1">
       <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" />
       </div>
-      <div className="col-span-1">
-      <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" />
+      <div className="col-span-1 mt-2 text-xs">
+      {/* <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" /> */}
+      7
       </div>
       <div className="col-span-1">
       <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" />
       </div>
-      <div className="col-span-1">
-      <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" />
+      <div className="col-span-1 mt-2 text-xs">
+      {/* <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" /> */}
+      9
       </div>
       <div className="col-span-1">
       <Image width={4} height={4} src="/verticalLine.svg" alt="1 hour" />
