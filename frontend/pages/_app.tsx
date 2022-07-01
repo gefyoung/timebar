0b638805da839717e1e2e5a1c987306c00729ff4
gty <gefyoung@gmail.com>
2022-06-30 18:50:02 -0600

    adding fathom

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,14 +1,39 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
+
+
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load('PGUABNQP', {
+      includedDomains: ['timebar.me'],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, [])
+
   return <>
     <Head>
       <meta name='application-name' content='Timebar' />
       <meta name='apple-mobile-web-app-capable' content='yes' />
       <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-      <meta name='apple-mobile-web-app-title' content='PWA App' />
-      <meta name='description' content='Track your habits, change your life' />
+      <meta name='apple-mobile-web-app-title' content='Timebar' />
+      {/* <meta name='description' content='Track your habits, change your life' /> */}
       <meta name='format-detection' content='telephone=no' />
       <meta name='mobile-web-app-capable' content='yes' />
       {/* <meta name='msapplication-config' content='/icons/browserconfig.xml' /> */}
@@ -16,30 +41,30 @@ function MyApp({ Component, pageProps }: AppProps) {
       <meta name='msapplication-tap-highlight' content='no' />
       <meta name='theme-color' content='#000000' />
 
-      {/* <link rel='apple-touch-icon' href='/icons/touch-icon-iphone.png' /> */}
-      {/* <link rel='apple-touch-icon' sizes='152x152' href='/icons/touch-icon-ipad.png' />
+      {/* <link rel='apple-touch-icon' href='/icons/touch-icon-iphone.png' />
+      <link rel='apple-touch-icon' sizes='152x152' href='/icons/touch-icon-ipad.png' />
       <link rel='apple-touch-icon' sizes='180x180' href='/icons/touch-icon-iphone-retina.png' />
       <link rel='apple-touch-icon' sizes='167x167' href='/icons/touch-icon-ipad-retina.png' /> */}
 
-      <link rel='icon' type='image/png' sizes='32x32' href='/pageScreenshot.png' />
-      <link rel='icon' type='image/png' sizes='16x16' href='/pageScreenshot.png' />
+      <link rel='icon' type='image/png' sizes='32x32' href='/files.svg' />
+      <link rel='icon' type='image/png' sizes='16x16' href='/files.svg' />
       <link rel='manifest' href='/manifest.json' />
       {/* <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' /> */}
       <link rel='shortcut icon' href='/files.svg' />
       <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
 
-      <meta name='twitter:card' content='Track your habits, change your life' />
+      <meta name='twitter:card' content='summary' />
       <meta name='twitter:url' content='https://timebar.me' />
       <meta name='twitter:title' content='Timebar' />
-      <meta name='twitter:description' content='Track your habits, change your life' />
-      <meta name='twitter:image' content='https://yourdomain.com/files.svg' />
+      {/* <meta name='twitter:description' content='Track your habits, change your life' /> */}
+      <meta name='twitter:image' content='https://timebar.me/pageScreenshot.png' />
       <meta name='twitter:creator' content='@_gty__' />
       <meta property='og:type' content='website' />
       <meta property='og:title' content='Timebar' />
-      <meta property='og:description' content='Track your habits, change your life' />
+      {/* <meta property='og:description' content='Track your habits, change your life' /> */}
       <meta property='og:site_name' content='Timebar' />
       <meta property='og:url' content='https://timebar.me' />
-      {/* <meta property='og:image' content='https://yourdomain.com/icons/apple-touch-icon.png' /> */}
+      <meta property='og:image' content='https://timebar.me/pageScreenshot.png' />
     </Head>
     <Component {...pageProps} /></>
 }
